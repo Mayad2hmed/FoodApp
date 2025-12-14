6b413ef05a65617bc473d1f2baa2617fd5c4c833
Mayada2hmed <eng.mayada.ahmed.elnagar@gmail.com>
2025-12-14 16:33:46 +0200

    Resolve merge conflict

--- src/DashboardModule/components/Dashboard/Dashboard.jsx
+++ src/DashboardModule/components/Dashboard/Dashboard.jsx
@@ -8,13 +8,13 @@ export default function Dashboard() {
   return (
     <>
       <Header title={
-        <h4 className="m-0 flex-1">
+        <h4 className="" >
     <span className="text-white fw-bold">Welcome</span>{' '}
     <span className="textSpan fw-light">Upskilling !</span>
   </h4>
       } description={'This is a welcoming screen for the entry of the application , you can now see the options'} imgUrl={headerImg}></Header>
-     <div className="home-datails d-flex justify-content-between align-items-center p-4 m-3">
-      <div className="caption">
+     <div className="home-datails d-flex justify-content-between align-items-center p-4 m-3" >
+      <div className="caption"style={{marginLeft:"250px"}} >
          <h3> <span className="text-black fw-light">Fill</span>{' '}
     <span className="text-success fw-light">the Recipes !</span></h3>
          <p>you can now fill the meals easily using the table and form , click here and sill it with the table !</p>
