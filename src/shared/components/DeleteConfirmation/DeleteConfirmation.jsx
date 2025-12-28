68420a5c9e53893232af0919e123d22ce90fbd07
Mayada2hmed <eng.mayada.ahmed.elnagar@gmail.com>
2025-12-28 04:17:12 +0200

    Update project files

--- src/shared/components/DeleteConfirmation/DeleteConfirmation.jsx
+++ src/shared/components/DeleteConfirmation/DeleteConfirmation.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import noData from "../../../assets/images/noData.png"
+import deletImg from "../../../assets/images/delet.png"
 export default function DeleteConfirmation({deleteItem,name}) {
   return (
     <div className='text-center p-3'>
-      <img src={noData} alt=''></img>
+      <img src={deletImg} alt=''></img>
       <h4 className='my-2'>Delete This{name}{deleteItem}?</h4>
       <h6>are you sure you want to delete this item ? if you are sure just click on delete it</h6>
       
